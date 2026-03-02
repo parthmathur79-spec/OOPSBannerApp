d94955d742a04e3e8ad132bc9eb494ec5ed365a9
Your Name <your-email@example.com>
2026-03-03 02:40:59 +0530

    UC2: Display OOPS as ASCII banner using manual print statements

---
+++ OOPSBannerAppUC2.java
@@ -0,0 +1,21 @@
+/**
+ * UC2: Display "OOPS" in large ASCII banner format.
+ * Uses manual print statements (no loops).
+ *
+ * @author Parth Mathur
+ * @version 2.0
+ */
+
+public class OOPSBannerAppUC2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    *****   ***** ");
+        System.out.println("*     *  *     *  *     *  *     ");
+        System.out.println("*     *  *     *  *     *  *     ");
+        System.out.println("*     *  *     *  *     *   *****");
+        System.out.println("*     *  *     *  *     *        *");
+        System.out.println("*     *  *     *  *     *        *");
+        System.out.println(" *****    *****    *****   ***** ");
+    }
+}
